ca5e4c46b801e9b404828ea454e13aa1e5989201
XingRay <leixing1012@gmail.com>
2024-08-03 08:45:05 +0800

    init

--- app/src/test/kotlin/org/example/AppTest.kt
+++ app/src/test/kotlin/org/example/AppTest.kt
@@ -10,6 +10,5 @@ import kotlin.test.assertNotNull
 class AppTest {
     @Test fun appHasAGreeting() {
         val classUnderTest = App()
-        assertNotNull(classUnderTest.greeting, "app should have a greeting")
     }
 }
